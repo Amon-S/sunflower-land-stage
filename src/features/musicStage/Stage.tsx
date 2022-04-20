5202cd8f39829b699097344ecf8eb13c6616d230
Shigo <murad@Gmail.com>
2022-04-20 23:18:56 +0100

    commit 2

--- src/features/musicStage/Stage.tsx
+++ src/features/musicStage/Stage.tsx
@@ -2,13 +2,16 @@ import React, { useContext, useState, useEffect } from "react";
 import { useActor } from "@xstate/react";
 
 import classNames from "classnames";
-
+import "./stage.css";
 import { Context } from "features/game/GameProvider";
 
 import { surpriseAudio } from "lib/utils/sfx";
 import stage from "assets/buildings/New_Stage_SFL.gif";
 import fireworks from "assets/decorations/firework.gif";
 import { GRID_WIDTH_PX } from "features/game/lib/constants";
+
+import Romy from "assets/npcs/stage/Romy.gif";
+import devtest from "assets/npcs/stage/devtest.gif";
 
 type Props = {};
 
@@ -44,13 +47,26 @@ export default function Stage() {
           "hover:img-highlight": isNotReadOnly,
         })}
       >
-        <img
-          src={stage}
-          alt="stage"
-          style={{ transform: "scale(2)", position: "relative", zIndex: 2 }}
-          onClick={fireWorks}
-          className="w-full"
-        />
+        <div onClick={fireWorks}>
+          <img
+            src={stage}
+            alt="stage"
+            style={{ transform: "scale(2)", position: "relative", zIndex: 2 }}
+            className="w-full"
+          />
+
+          <img
+            src={Romy}
+            alt=""
+            style={{
+              transform: "scale(0.6)",
+              position: "relative",
+              top: "-200px",
+              left: "60px",
+              zIndex: 2,
+            }}
+          />
+        </div>
 
         {isOpen && (
           <img
@@ -70,6 +86,16 @@ export default function Stage() {
             }}
             alt="fireworks"
             className="fireworks1"
+          />
+        )}
+        {isOpen && (
+          <img
+            src={devtest}
+            style={{
+              transform: "translate(60px,-160px), scale(0.7)",
+            }}
+            id="steve"
+            alt="steve"
           />
         )}
       </div>
